cb463ef2d6176368b5a93463265aa020d41a152d
gofujita <gofujita@me.com>
2015-11-04 18:37:59 +0900

    initial commit to pt.pl and pt01.pl

---
+++ pt.pl
@@ -0,0 +1,5 @@
+$x = "12.3";
+$weight = $x . " kg";   #  $weight の値は "12.3 kg"
+print $weight, "\n";    #  $weight の値を画面に出力．"\n"は改行．
+$x_doubled = $x * 2.0;  #  $x_doubled の値は24.6
+print $x_doubled, "\n"; #  $x_doubled の値を画面に出力．
---
+++ pt01.pl
@@ -0,0 +1,11 @@
+$x = 2;
+
+if ($x == 3) {
+	print "x is 3.\n";
+}
+elsif ($x > 0) {
+	print "x is larger than 0, but not 3.\n"
+}
+else {
+	print "x is not larger than 0.\n"
+}
